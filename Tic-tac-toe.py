2489f66e83f9b661a502969599bd4eb58ba59e30
Paige Bolduc <bolducp@users.noreply.github.com>
2016-02-14 21:03:31 -0800

    Update Tic-tac-toe.py

--- Tic-tac-toe.py
+++ Tic-tac-toe.py
@@ -1,77 +1,77 @@
-
-
 positions = ["_1_|", "_2_", "|_3_", "_4_|", "_5_", "|_6_", " 7 |", " 8 ", "| 9 "]
 
+
 def print_board():
-    for i in xrange(9):
-        print positions[i],
-        if (i + 1) % 3 == 0:
+    for index in range(9):
+        print positions[index],
+        if (index + 1) % 3 == 0:
             print
 
 
-def get_user1_move():
-    move = int(raw_input("Player 1: Choose a position \n"))
-    if move in range(1, 10) and positions[move - 1] != " X " and positions[move - 1] != " O ":
-        positions[move - 1] = " X "
-    else:
-        print "Please enter a valid position"
-        return get_user1_move()
+def make_user_move(player_symbol):
+    try:
+        move = int(raw_input("\nPlayer %s: Choose a position:  \n" % player_symbol))
+        if move in range(1, 10) and "X" not in positions[move - 1] and "O" not in positions[move - 1]:
+            positions[move - 1] = " %s " % player_symbol
+        else:
+            print "Please enter a valid position"
+            return make_user_move(player_symbol)
+    except ValueError:
+        print "Please enter a valid numeric position"
+        return make_user_move(player_symbol)
 
 
-def get_user2_move():
-    move = int(raw_input("Player 2: Choose a position \n"))
-    if move in range(1, 10) and positions[move - 1] != " X " and positions[move - 1] != " O ":
-        positions[move - 1] = " O "
-    else:
-        print "Please enter a valid position"
-        return get_user2_move()
+def game_has_been_won(player_symbols):
+    for player_symbol in player_symbols:
+        if player_has_won(player_symbol):
+            return True
+    return False
 
 
 def player_has_won(player_symbol):
     winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for combo in winning_combos:
-        player_symbol_count = 0
-        for index in combo:
-            if player_symbol in positions[index]:
-                player_symbol_count += 1
-        if player_symbol_count == 3:
+        if player_has_combo(player_symbol, combo):
             return True
     return False
 
 
+def player_has_combo(player_symbol, combo):
+    for index in combo:
+        if player_symbol not in positions[index]:
+            return False
+    return True
+
+
 def no_moves_remain():
     for position in positions:
-        if position != " X " and position != " O ":
+        if "X" not in position and "O" not in position:
             return False
     return True
 
 
 def print_outcome():
-    if no_moves_remain():
-        print "No moves remain. It's a tie."
-    elif player_has_won("X"):
-        print "Player 1 wins!"
+    if player_has_won("X"):
+        print "Player X wins!"
     elif player_has_won("O"):
-        print "Player 2 wins!"
+        print "Player O wins!"
+    elif no_moves_remain():
+        print "No moves remain. It's a tie."
     else:
         print "Something went wrong."
 
 
 def main():
     print_board()
+    player_symbols = ["X", "O"]
 
     while True:
-        if player_has_won("x") or player_has_won("O") or no_moves_remain():
-            print_outcome()
-            break
-        get_user1_move()
-        print_board()
+        for player_symbol in player_symbols:
+            if game_has_been_won(player_symbols) or no_moves_remain():
+                print_outcome()
+                return
 
-        if player_has_won("x") or player_has_won("O") or no_moves_remain():
-            print_outcome()
-            break
-        else:
-            get_user2_move()
+            make_user_move(player_symbol)
             print_board()
 
 
